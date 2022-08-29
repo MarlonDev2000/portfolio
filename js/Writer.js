cf7f26d5f9d5f6f8d4cdfeadfff98e8573786349
MarlonDev2000 <marloncs13@outlook.com>
2022-08-29 12:01:20 -0300

    initial commit

---
+++ js/Writer.js
@@ -0,0 +1,17 @@
+function Writer(e){
+  const textoArray = e.innerHTML.split('');
+  e.innerHTML = ' ';
+  textoArray.forEach(function(letra, i){   
+    setTimeout(() => {
+      e.innerHTML += letra;
+    }, 250 * i)
+  });
+}
+const titulo = document.querySelector('#home-text--span');
+Writer(titulo);
+
+setInterval(()=>{
+  Writer(titulo)
+  console.log('1')
+}, 8000)
+
