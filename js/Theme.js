0731cb5e551ff9d544829909431b996265c56921
MarlonDev2000 <marloncs13@outlook.com>
2022-09-19 15:10:58 -0300

    adicionando modo light

---
+++ js/Theme.js
@@ -0,0 +1,60 @@
+let theme = document.querySelector('#theme-change')
+
+const colors = {
+  primaryBlack: '#131515',
+  primaryGray: '#2B2C28',
+  primaryWhite: '#F9F9F9',
+  bgGray: '#eee',
+  rgba: 'rgba(0, 0, 0, 0.2)'
+}
+
+
+let themeWhite = false
+
+theme.addEventListener('click', () => {
+  if(themeWhite === false) {
+    document.body.style.backgroundColor = colors.primaryWhite
+    document.querySelector('#theme-image').setAttribute('src', './images/moon.svg')
+    document.querySelector('#home').style.color = colors.primaryGray  
+    let homeLinks = document.querySelectorAll('.home-links a')
+    homeLinks.forEach((item)=>{
+      item.style.color = colors.primaryGray
+    })
+    document.querySelector('#about').style.backgroundColor = colors.bgGray
+    document.querySelector('#about').style.color = colors.primaryGray
+    let abilitities = document.querySelectorAll('.abilities-item')
+    abilitities.forEach((item)=>{
+      item.style.backgroundColor = colors.primaryGray
+    })
+    let gridItem = document.querySelectorAll('.grid-item')
+    gridItem.forEach((item)=>{
+      item.style.backgroundColor = colors.primaryGray
+    })
+    console.log(themeWhite)
+    themeWhite = true
+  } else {
+    document.body.style.backgroundColor = colors.primaryBlack
+    document.querySelector('#theme-image').setAttribute('src', './images/sun.svg')
+    document.querySelector('#home').style.color = colors.primaryWhite
+    let homeLinks = document.querySelectorAll('.home-links a')
+    homeLinks.forEach((item)=>{
+      item.style.color = colors.primaryWhite
+    })
+    document.querySelector('#about').style.backgroundColor = colors.primaryGray
+    document.querySelector('#about').style.color = colors.primaryWhite
+    let abilitities = document.querySelectorAll('.abilities-item')
+    abilitities.forEach((item)=>{
+      item.style.backgroundColor = colors.rgba
+    })
+    let gridItem = document.querySelectorAll('.grid-item')
+    gridItem.forEach((item)=>{
+      item.style.backgroundColor = colors.rgba
+    })
+    console.log(themeWhite)
+    themeWhite = false
+  }
+})
+
+
+
+
